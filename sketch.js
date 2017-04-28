57b49f565b572d4c84daab90fcd2c556f45887eb
Chris Trowell <chris.a.trowell@gmail.com>
2017-04-27 20:41:10 -0400

    Added initial empty p5 project files
    Made small modification: added keyInput() function.

---
+++ sketch.js
@@ -0,0 +1,33 @@
+function setup() {
+
+}
+
+function draw() {
+  
+}
+
+
+
+
+
+
+
+function keyInput(){
+	if (keyIsPressed === true) {
+		
+		if(keyCode === UP_ARROW) {
+			// Code to run when we press UP_ARROW
+			// player.move(0,-5)
+		} else if (keyCode === DOWN_ARROW) {
+			// player.move(0,5)
+		} 
+
+
+		if (keyCode === LEFT_ARROW) {
+			// player.move(-5,0)
+		} else if (keyCode === RIGHT_ARROW) {
+			// player.move(5,0)
+		}
+
+	}
+}
